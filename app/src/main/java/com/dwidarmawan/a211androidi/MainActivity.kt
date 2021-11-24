2cbe81e2b56394c57aaf97565fe162671d53e2fc
dwidarmawan <81225366+dwidarmawan@users.noreply.github.com>
2021-11-23 22:33:38 -0800

    menghubungkan kotlin dengan komponen

--- app/src/main/java/com/dwidarmawan/a211androidi/MainActivity.kt
+++ app/src/main/java/com/dwidarmawan/a211androidi/MainActivity.kt
@@ -2,10 +2,34 @@ package com.dwidarmawan.a211androidi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val nomorEditText = findViewById<EditText>(R.id.nomorEditText)
+        val tanggallahirEditText = findViewById<EditText>(R.id.tanggallahirEditText)
+        val jumlahmkEditText = findViewById<EditText>(R.id.jumlahmkEditText)
+
+        simpanButton.setOnClickListener {
+            namaEditText.setText("")
+            emailEditText.setText("")
+            nomorEditText.setText("")
+            tanggallahirEditText.setText("")
+            jumlahmkEditText.setText("")
+        }
+
+        batalButton.setOnClickListener{
+            finish()
+
+        }
     }
 }
